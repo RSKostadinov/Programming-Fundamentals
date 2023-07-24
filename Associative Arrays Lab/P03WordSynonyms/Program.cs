30922df24bf7fff079e7a91f50f361077eb35be2
Radoslav Stankov Kostadinov <rkostadinov1337@gmail.com>
2023-07-24 16:04:17 +0300

    Added more solutions

---
+++ Associative Arrays Lab/P03WordSynonyms/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+internal class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        var words = new Dictionary<string, List<string>>();
+
+        for (int i = 0; i < n; i++)
+        {
+            string key = Console.ReadLine();
+            string value = Console.ReadLine();
+            if (!words.ContainsKey(key))
+            {
+                words.Add(key, new List<string>());
+            }
+
+            words[key].Add(value);
+
+        }
+
+        foreach (var kvp in words)
+        {
+            string word = kvp.Key;
+            List<string> values = kvp.Value;
+
+            string valuesString = String.Join(", ", values);
+
+            Console.WriteLine($"{word} - {valuesString}");
+        }
+    }
+}
+
+
---
+++ Associative Arrays Lab/P04WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+internal class Program
+{
+    static void Main()
+    {
+        string[] words = Console
+            .ReadLine()
+            .Split()
+            .Where(w => w.Length % 2 == 0)
+            .ToArray();
+
+        Console.WriteLine(string.Join('\n', words));
+    }
+}
+
